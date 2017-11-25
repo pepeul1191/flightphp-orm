02d8036829c5554260b15086ab5dfba990290cfc
Pepe Valdivia <jvaldivia@softweb.pe>
2017-11-25 17:16:35 -0500

    Hola mundo

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+require 'app/vendor/autoload.php';
+
+header('x-powered-by: PHP');
+header('Server: Ubuntu');
+header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+header('Content-type: text/html; charset=UTF-8');
+
+Flight::route('/hola', function(){
+  echo 'hello world!';
+});
+
+Flight::start();
+
+?>
